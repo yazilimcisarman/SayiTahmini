6502e270299a25b558a542c3a68c83947f8a9144
Muhammet Seyda Armağan <muhammedseyda@hotmail.com>
2024-04-26 20:41:55 +0300

    .

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SayiTahmini
         static void Main(string[] args)
         {
             Console.WriteLine("Sayı tahmin uygulamasına hoşgeldin.");
+            Console.WriteLine("3 hakkınız vardır !!!");
             Console.WriteLine("Sayı 0 ile 20 arasında bir sayı.");
             Console.WriteLine("Tahminini gir:");
             Random random = new Random();
